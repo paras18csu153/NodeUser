3cfb5982092e3ca283042ab1ed3ca6c274eab1d1
paras18csu153 <paras18csu153@ncuindia.edu>
2022-01-26 11:03:29 +0530

    Changed Token expiry

--- helpers/tokenGenerator.js
+++ helpers/tokenGenerator.js
@@ -4,7 +4,7 @@ function tokenGenerator(username) {
   // Generate JWT token
   var generatedToken = jwt.sign(
     {
-      exp: Math.floor(Date.now() / 1000) + 1,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
       username: username,
     },
     "IamGeneratedForNodeUserApplication"
