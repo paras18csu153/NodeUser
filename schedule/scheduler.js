14e60672656fe73b84c4caafaa12ecfabebe1861
paras18csu153 <paras18csu153@ncuindia.edu>
2022-01-27 13:15:27 +0530

    Changed Display Message.

--- schedule/scheduler.js
+++ schedule/scheduler.js
@@ -3,7 +3,6 @@ const Verification = require("../models/verification.model");
 
 function scheduler() {
   const job = schedule.scheduleJob("30 2 * * *", function () {
-    console.log("The answer to life, the universe, and everything!");
     var currentTimestamp = Date.now();
     Verification.deleteAllByTime(currentTimestamp);
   });
